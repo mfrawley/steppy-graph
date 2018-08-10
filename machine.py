2fc55a09628104f9ca342a3c8240ca393ef5efcf
Mark Frawley <markfrawley@gmail.com>
2018-08-10 13:27:13 +0200

    tests passing

--- machine.py
+++ machine.py
@@ -1,17 +1,19 @@
 import json
 from typing import List
 
-from states import State, JSON_INDENT
+from states import State, JSON_INDENT, Task, StateEncoder
 
 
 class StateMachine:
     def __init__(self) -> None:
+        self.States = None
         self.StartAt: str = None
         self._states: List[State] = []
 
     def to_json(self) -> str:
         return json.dumps(self,
                           sort_keys=True,
+                          # skip_keys=True,
                           indent=JSON_INDENT,
                           cls=StateMachineEncoder)
 
@@ -19,10 +21,9 @@ class StateMachine:
         """
         This method
         """
-
-        self._states.append(state)
         if len(self._states) > 0:
             self._states[-1].Next = state.name()
+        self._states.append(state)
         return self
 
     def build(self) -> object:
@@ -35,17 +36,36 @@ class StateMachine:
 
             if i == states_len - 1:
                 s.End = True
-            d[s.name()] = s.to_json()
+            d[s.name()] = s
         self.States = d
-        del self._states
         return self
+
+    def get_states(self) -> List[State]:
+        return self._states
+
+    def printable(self) -> str:
+        # return self.build().States
+        return ' '.join([str(s.__dict__) for k, s in self.build().States.items()])
 
 
 class StateMachineEncoder(json.JSONEncoder):
     def default(self, obj):
-        print(type(obj))
+        if isinstance(obj, StateMachine):
+            del obj._states
+
+            for k, s in obj.States.items():
+                obj.States[k] = StateEncoder().default(s)
+                # return obj
+
         try:
             return super(StateMachineEncoder, self).default(obj)
         except Exception as e:
             print(e)
             print(obj.__dict__)
+
+
+if __name__ == "__main__":
+    s = StateMachine()
+    s.next(Task(resource="some", name="Kermit", comment='Foo'))
+    s.build()
+    print(s.to_json())
--- test/machine_test.py
+++ test/machine_test.py
@@ -1,7 +1,24 @@
-from states import Task
+import json
+
 from machine import StateMachine
+from states import Task
+
 
 def test_hello_machine():
     s = StateMachine()
     s.next(Task(resource="some", name="Kermit", comment='Foo'))
-    assert len(s.States) == 1
+    s.build()
+    assert len(s.get_states()) == 1
+    assert s.StartAt == s.get_states()[0].name(), str(s.get_states()[0])
+
+
+def test_next_set_for_two_state_machine():
+    s = StateMachine()
+    s.next(Task(resource="some", name="Kermit", comment='Foo'))
+    s.next(Task(resource="some", name="Miss Piggy", comment='Foo'))
+    s.build()
+    assert len(s.get_states()) == 2
+    assert s.StartAt == "Kermit"
+    assert s.get_states()[0].Next == s.get_states()[1].name(), s.printable()
+    assert s.get_states()[-1].Next == None
+    # assert s.get_states()[0].Next == s.get_states()[1].name()
